76170fd54b7a402ace1d57343e5f642c98bef3b8
mizza-cz <mizza.cz666@gmail.com>
2022-03-21 07:33:35 +0100

    add fix

--- src/js/custom/select2.js
+++ src/js/custom/select2.js
@@ -2,18 +2,4 @@ $('.js-select').select2({
    width: '100%',
    minimumResultsForSearch: -1
  });
- function formatImage (image) {
-   if (!image.id) {
-     return image.text;
-   }
-   var baseUrl = "images/content";
-   var $image = $(
-     '<span><img src="' + baseUrl + '/' + image.element.value.toLowerCase() + '.png" class="select-img" /> ' + image.text + '</span>'
-   );
-   return $image;
- };
- $('.js-select-with-image').select2({
-   width: '100%',
-   minimumResultsForSearch: -1,
-   templateResult: formatImage
- });
+
