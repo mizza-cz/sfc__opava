3796abc9beb94e6528776e9d5baac7c0f20cff0f
mizza-cz <mizza.cz666@gmail.com>
2022-06-09 10:17:11 +0200

    add fix

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,8 +3,8 @@
 //=include ../../node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js
 //=include ../../node_modules/select2/dist/js/select2.min.js
 //=include ../../node_modules/slick-carousel/slick/slick.js
-//=include ../../node_modules/photoswipe/dist/photoswipe.js
-//=include ../../node_modules/photoswipe/dist/photoswipe-ui-default.js
+//=include ../../node_modules/lightgallery/lightgallery.umd.js
+//=include ../../node_modules/lightgallery/lightgallery.min.js
 document.addEventListener("DOMContentLoaded", function() {
 //=include ../components/**/*.js
 //=include custom/*.js
